a88adaf4a177a289c91b51391e99d6c8466ff1f0
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-10-25 15:40:18 +0700

    P7

--- Week5/Week5/Program.cs
+++ Week5/Week5/Program.cs
@@ -4,18 +4,9 @@ public class TryCatch
     static void Main(string[] args)
     {
         int a = 0;
-        try
-        {
-            Console.WriteLine(100 / a);
-        }
-        catch (NullReferenceException e)
-        {
-            Console.WriteLine(e.Message);
-        }
-        catch (DivideByZeroException e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        int b = 10;
+        b /= a;
+        Console.WriteLine(a);
         Console.ReadKey();
     }
 }
