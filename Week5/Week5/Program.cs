1a8b600c76f1f32164bcbb4d3f32c820d5e44373
peeraphonza2143 <53931459+peeraphonza2143@users.noreply.github.com>
2019-10-25 15:56:26 +0700

    P22 P23

--- Week5/Week5/Program.cs
+++ Week5/Week5/Program.cs
@@ -2,14 +2,20 @@
 
 namespace ConsoleApp2
 {
-    class Multiply
+    class BreakTest
     {
         static void Main()
         {
-            for (int i = 1; i <= 12; i++)
+            for (int i = 1; i <= 100; i++)
             {
-                Console.WriteLine("{0, 2} x 2 = {1, 2}", i, i * 2);
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
             }
+            // Keep the console open in debug mode.
+            Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
     }
