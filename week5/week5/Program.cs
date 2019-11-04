6426648417c2179e9962fcc3b61c9dd6bf524418
maethapon1999 <61030045@kmitl.ac.th>
2019-11-04 19:08:20 +0700

    add

--- week5/week5/Program.cs
+++ week5/week5/Program.cs
@@ -5,15 +5,16 @@ namespace week5
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
         {
-            int value = 800000000;
-            checked // check for overflow
-            {
-                int square = value * value;
-                Console.WriteLine("{0} ^ 2 = {1}", value, square);
+            int a = 10;
+            int b = 20;
+            int c = add(a, b);
+        }
+        private static int add(int a, int b)
+        {
+            throw new NotImplementedException();
 
-            }
         }
     }
-}
+    }
