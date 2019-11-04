2571e9efa55fb55d49a126c5c7038b9b7d6d8f9a
maethapon1999 <61030045@kmitl.ac.th>
2019-11-04 19:37:42 +0700

    add

--- week5/week5/Program.cs
+++ week5/week5/Program.cs
@@ -7,28 +7,18 @@ namespace week5
     {
         public static void Main()
         {
-            Console.WriteLine("---- Begin of While statement.");
-            int n = 1;
-            while (n < 1)
-            {
-                Console.WriteLine(" Current value of n is {0}", n);
-                n++;
-            }
-            Console.WriteLine("---- End of While statement.");
 
-            Console.WriteLine("---- Begin of Do..While statement.");
-            n = 1;
-            do
             {
-                Console.WriteLine(" Current value of n is {0}", n);
-                n++;
-            }
-            while (n < 1);
-            Console.WriteLine("---- End of Do..While statement.");
+                for (int i = 1; i <= 5; i++)
+                {
+                    Console.WriteLine(i);
 
+                }
+            }
         }
     }
-    }
+}
+    
 
 
         
