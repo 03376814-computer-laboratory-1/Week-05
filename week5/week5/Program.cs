ecba2a71f8eb1b9c3b9ebf1c1972bd8538880551
maethapon1999 <61030045@kmitl.ac.th>
2019-11-04 18:57:08 +0700

    add

--- week5/week5/Program.cs
+++ week5/week5/Program.cs
@@ -7,19 +7,9 @@ namespace week5
     {
         static void Main(string[] args)
         {
-            int a = 0;
-            try
-            {
-                Console.WriteLine(100 / a);
-            }
-            catch (NullReferenceException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (DivideByZeroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            int a = int.MaxValue;
+            a *= 2;
+            Console.WriteLine(a);
         }
     }
 }
